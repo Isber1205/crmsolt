d417e814adfd0951b0d8565fb47516df08e9abc4
Isber1205 <item1495@gmail.com>
2024-11-30 22:24:11 -0500

    "update login production"

--- app/actions.ts
+++ app/actions.ts
@@ -272,8 +272,8 @@ export async function checkOut() {
         const session = await stripe.checkout.sessions.create({
             mode: "payment",
             line_items: lineItems,
-            success_url: "http://localhost:3000/payment/success",
-            cancel_url: "http://localhost:3000/payment/cancel",
+            success_url: "https://crmsolt.vercel.app/payment/success",
+            cancel_url: "https://crmsolt.vercel.app/payment/cancel",
             metadata: {
                 userId: user.id,
             }
